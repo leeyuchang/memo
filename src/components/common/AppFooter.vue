64a38cb58232d0c27eb2f306d6ff1ab370021eec
leeyuchang <leeyucha@gmail.com>
2020-05-05 10:14:47 +0900

    make it better

---
+++ src/components/common/AppFooter.vue
@@ -0,0 +1,29 @@
+<template>
+	<footer>
+		<div>© 2020</div>
+	</footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+	/* position: fixed;
+	bottom: 0;
+	width: 100%; */
+	height: 52px;
+	padding: 0 20px;
+	margin-top: 50px;
+	font-size: 14px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background: #364f6b;
+	color: white;
+}
+footer a {
+	color: white;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+// 	devServer: {
+// 		overlay: false,
+// 	},
+// };
+
+module.exports = {
+	publicPath: '/memo/',
+	outputDir: 'dist',
+};
